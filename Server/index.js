ba9e4ecda0f4f7b5ab5f2841621d58f0ae744843
Vinitpal <vs361017@gmail.com>
2021-12-06 12:58:47 +0530

    initial commit

---
+++ Server/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+
+// Routes
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+
+const app = express();
+const PORT = process.env.PORT || 8800;
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+// middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+// Api routes
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to homepage");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
